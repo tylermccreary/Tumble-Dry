66cb6cd3ef0111b6ac6fb06cd3de1cdfac19bed0
tmccreary <tjmccreary@gmail.com>
2014-09-10 13:45:05 -0500

    shock

---
+++ Assets/Scripts/Shock.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shock : MonoBehaviour {
+	
+	private float shockRadius;
+	public LayerMask player;
+	private bool shock;
+	private GameObject enemy;
+	
+	void Start()
+	{
+		shockRadius = transform.GetComponent<CircleCollider2D> ().radius;
+	}
+	
+	void Update()
+	{
+		shock = Physics2D.OverlapCircle (transform.position, shockRadius, player);
+		if (shock) 
+		{
+			Destroy(gameObject);
+			//find and destroy all enemies
+			object[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+			foreach(object thisObject in allEnemies)
+			{
+				enemy = (GameObject) thisObject;
+				LintExplosion.Instance.Explosion (enemy.gameObject.transform.position);
+				Destroy(enemy);
+			}
+				
+		}
+	}
+}
