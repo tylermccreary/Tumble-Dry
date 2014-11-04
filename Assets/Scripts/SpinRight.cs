94342d57e2e18b5d8a15047b4ce8af1989250232
tmccreary <tjmccreary@gmail.com>
2014-11-04 17:00:16 -0600

    level 4

--- Assets/Scripts/SpinRight.cs
+++ Assets/Scripts/SpinRight.cs
@@ -5,10 +5,15 @@ public class SpinRight : MonoBehaviour
 {
 
 		public static readonly int speed = 10;
+		public bool right;
 
 		// Update is called once per frame
 		void FixedUpdate ()
 		{
-				transform.Rotate (Vector3.back * Time.deltaTime * speed);
+				if (right) {
+						transform.Rotate (Vector3.back * Time.deltaTime * speed);
+				} else {
+						transform.Rotate (Vector3.forward * Time.deltaTime * speed);
+				}
 		}
 }
---
+++ Assets/Scripts/StationaryUpDown.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StationaryUpDown : MonoBehaviour
+{
+		public bool goUp;
+		public float distance;
+		public float speed;
+		// Use this for initialization
+		IEnumerator Start ()
+		{
+		float interval = 30 / speed;
+				Vector2 pointB;
+				if (goUp) {
+						pointB = new Vector2 (transform.position.x, transform.position.y + distance);
+				} else {
+						pointB = new Vector2 (transform.position.x, transform.position.y - distance);
+				}
+				Vector2 pointA = transform.position;
+				while (true) {
+						yield return StartCoroutine (MoveObject (transform, pointA, pointB, interval));
+						yield return StartCoroutine (MoveObject (transform, pointB, pointA, interval));
+				}
+		}
+	
+		// Update is called once per frame
+		IEnumerator MoveObject (Transform thisTransform, Vector2 startPos, Vector2 endPos, float time)
+		{
+				float i = 0.0f;
+				float rate = 1.0f / time;
+				while (i < 1.0f) {
+						i += Time.deltaTime * rate;
+						this.transform.position = Vector2.Lerp (startPos, endPos, i);
+						yield return null;
+				}
+		}
+}
