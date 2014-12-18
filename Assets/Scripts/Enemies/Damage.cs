b1317968840b61ab1ec2a67b73f4bcc47e7d101b
tmccreary <tjmccreary@gmail.com>
2014-12-18 13:51:47 -0600

    added box and animation

---
+++ Assets/Scripts/Enemies/Damage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+
+	public int hp;
+
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (coll.gameObject.layer == 8) {
+			SockyController.doDamage (hp);
+		}
+	}
+}
