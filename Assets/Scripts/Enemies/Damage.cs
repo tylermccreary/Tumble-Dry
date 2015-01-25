bd40ed27af49aaf5b1935ca4b2d569df10787f96
tmccreary <tjmccreary@gmail.com>
2015-01-25 16:21:02 -0600

    fixed sock jump anim

---
+++ Assets/Scripts/Enemies/Damage.cs
@@ -0,0 +1,14 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+	
+	public int hp;
+	
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (coll.gameObject.layer == 8) {
+			SockyController.doDamage (hp);
+		}
+	}
+}
